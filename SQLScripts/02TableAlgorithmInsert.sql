4c034a36d2ed036aa1d9224ac48bd769bba40785
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-01-16 22:06:58 -0600

    Initial algorithms catalogue insert performed

---
+++ SQLScripts/02TableAlgorithmInsert.sql
@@ -0,0 +1,7 @@
+#Initial insert of algorithms
+#First insert 2017-01-16
+INSERT INTO Algorithm(Name) VALUES ('Logistic Regression');
+INSERT INTO Algorithm(Name) VALUES ('Neural Network');
+INSERT INTO Algorithm(Name) VALUES ('Convolutional Neural Network');
+INSERT INTO Algorithm(Name) VALUES ('Recurrent Neural Network');
+INSERT INTO Algorithm(Name) VALUES ('Support Vector Machine');
