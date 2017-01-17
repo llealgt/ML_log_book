8ef6d6ac0664a48dc4e6fe961a797be28d7adbdb
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-01-16 22:37:46 -0600

    Created t ool  and tool insert scripts

---
+++ SQLScripts/04TableToolInsert.sql
@@ -0,0 +1,6 @@
+#Initial insert of Tools
+#First insert 2017-01-16
+INSERT INTO Tool(Name,SiteURL) VALUES ('Tensorflow','https://www.tensorflow.org/');
+INSERT INTO Tool(Name,SiteURL) VALUES ('Scikit-learn','http://scikit-learn.org/');
+INSERT INTO Tool(Name,SiteURL) VALUES ('Apache Spark','http://spark.apache.org/');
+
---
+++ SQLScripts/05TableToolFrameworkInsert.sql
@@ -0,0 +1,11 @@
+#Initial insert of tool frameworks(or libraries)
+#First insert 2017-01-16
+
+INSERT INTO ToolFramework(FKTool,Name) VALUES (1,'Pure Tensorflow');
+INSERT INTO ToolFramework(FKTool,Name) VALUES (1,'Tflearn');
+INSERT INTO ToolFramework(FKTool,Name) VALUES (1,'SKFlow');
+
+INSERT INTO ToolFramework(FKTool,Name) VALUES (2,'Pure Scikit-learn');
+
+INSERT INTO ToolFramework(FKTool,Name) VALUES (3,'Spark MLlib');
+INSERT INTO ToolFramework(FKTool,Name) VALUES (3,'Sparking Water');
