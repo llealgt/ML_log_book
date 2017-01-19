64495c4b6c9bd54cce2ffaebce0da5d4a7816f38
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-01-18 23:09:41 -0600

    added get_confusion_matrix function

--- machine_learning_framework/machine_learning_framework/performance_metrics/binary_classification_metric_calculator.py
+++ machine_learning_framework/machine_learning_framework/performance_metrics/binary_classification_metric_calculator.py
@@ -4,5 +4,16 @@
 import numpy as np
 
 class BinaryClassificationMetricCalculator:
-    def __init__self(self):
-        print "Empty object"
+    
+    def __init__(self,predictions,real_values):
+        self.predictions = predictions
+        self.real_values = real_values
+        
+        
+    def get_confusion_matrix(self):
+        true_positives =  np.sum( np.logical_and((self.predictions == 1),(self.real_values == 1)))
+        false_positives = np.sum(np.logical_and((self.predictions == 1),(self.real_values == 0)))
+        false_negatives = np.sum(np.logical_and((self.predictions == 0),(self.real_values == 1)))
+        true_negatives =  np.sum(np.logical_and((self.predictions == 0),(self.real_values == 0)))
+        
+        return true_positives,false_positives,true_negatives,false_negatives
