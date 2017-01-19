f1c3d06ebd99a413888f26c7fc1a4a43273c9100
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-01-18 22:11:47 -0600

    New fyle to contain binary classification metric calculator class

---
+++ machine_learning_framework/machine_learning_framework/performance_metrics/binary_classification_metric_calculator.py
@@ -0,0 +1,8 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+import numpy as np
+
+class BinaryClassificationMetricCalculator:
+    def __init__self(self):
+        print "Empty object"
