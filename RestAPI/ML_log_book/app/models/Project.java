ee8b8d3e7a4d00031b8b29371b42b55372090927
Luis Leal-Debian8 <wichofer89@gmail.com>
2017-02-14 22:32:50 -0600

    Finished Project.java model

--- RestAPI/ML_log_book/app/models/Project.java
+++ RestAPI/ML_log_book/app/models/Project.java
@@ -6,22 +6,62 @@ import play.data.format.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "Project", catalog = "ML_log_book")
 public class Project {
-	public static String TABLE = Project.class.getSimpleName();
+    public static String TABLE = Project.class.getSimpleName();
 	
-	@Id
+    @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    public Integer PKProject;
+    private Integer PKProject;
+    private String name;
+    private String contentDirectory;
+    private String description;
 
-    @Constraints.Required
-    public String Name;
-    
     public Project(){}
-    public Project(int id,String title){
-    	this.PKProject = id;
-    	this.Name = title;
+
+    public static String getTABLE() {
+        return TABLE;
     }
-   
+
+    public static void setTABLE(String TABLE) {
+        Project.TABLE = TABLE;
+    }
+
+    public Integer getPKProject() {
+        return PKProject;
+    }
+
+    public void setPKProject(Integer PKProject) {
+        this.PKProject = PKProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContentDirectory() {
+        return contentDirectory;
+    }
+
+    public void setContentDirectory(String contentDirectory) {
+        this.contentDirectory = contentDirectory;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
 }
